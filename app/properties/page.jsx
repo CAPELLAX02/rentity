75a3fa744695528b0f85298626b41add2772fd10
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-16 23:26:11 +0300

    PorpertyCard component completed

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found.</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            {properties.map((property) => {
+              return <PropertyCard key={property._id} property={property} />;
+            })}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;

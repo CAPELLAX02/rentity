e00d607788474d318717d967478debebc5f48751
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-17 05:08:32 +0300

    Google profile photo added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,8 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 const Navbar = () => {
   const { data: session } = useSession();
 
+  const profileImage = session?.user?.image;
+
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [providers, setProviders] = useState(null);
@@ -173,7 +175,9 @@ const Navbar = () => {
                     <span className='sr-only'>Open user menu</span>
                     <Image
                       className='h-8 w-8 rounded-full'
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
+                      width={40}
+                      height={40}
                       alt=''
                     />
                   </button>

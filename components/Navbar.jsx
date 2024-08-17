c6beea044b82cea1816cfc791f7235027e26f03e
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-17 05:13:15 +0300

    Log out functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -216,6 +216,10 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                     >
                       Sign Out
                     </button>

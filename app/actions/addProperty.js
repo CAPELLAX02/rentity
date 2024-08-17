3348e17c1f50548e3521a0825ef54e008fa3b5c4
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-17 15:08:41 +0300

    server actions started

---
+++ app/actions/addProperty.js
@@ -0,0 +1,5 @@
+'use server';
+
+export const addProperty = async (formData) => {
+  console.log(formData.get('name'));
+};
--- components/AddPropertyForm.jsx
+++ components/AddPropertyForm.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { addProperty } from '@/app/actions/addProperty';
 
 const AddPropertyForm = () => {
   return (
-    <form>
+    <form action={addProperty}>
       <h2 className='text-3xl text-center font-semibold mb-6'>Add Property</h2>
 
       <div className='mb-4'>

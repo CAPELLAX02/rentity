17e8b6aca76f5ae5bfd567a31354a120860408e6
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-17 04:53:29 +0300

    Google Auth log in button launched

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,31 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../assets/images/logo-white.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import profileDefault from '../assets/images/profile.png';
 import { FcGoogle } from 'react-icons/fc';
 import { usePathname } from 'next/navigation';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  console.log(session);
+
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [providers, setProviders] = useState(null);
 
   const pathname = usePathname();
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setAuthProviders();
+  }, []);
 
   return (
     <nav className='bg-blue-600'>
@@ -80,7 +92,7 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                {isLoggedIn && (
+                {session && (
                   <Link
                     href='/properties/add'
                     className={`${
@@ -97,19 +109,28 @@ const Navbar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          {!isLoggedIn && (
+          {!session && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
-                <button className='flex items-center text-amber-950 bg-amber-300 hover:bg-amber-950 hover:text-white rounded-md px-3 py-2 duration-200 font-bold'>
-                  <FcGoogle className='mr-2 text-xl' />
-                  <span>Login or Register</span>
-                </button>
+                {providers &&
+                  Object.values(providers).map((provider, index) => {
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => signIn(provider.id)}
+                        className='flex items-center text-amber-950 bg-amber-300 hover:bg-amber-950 hover:text-white rounded-md px-3 py-2 duration-200 font-bold'
+                      >
+                        <FcGoogle className='mr-2 text-xl' />
+                        <span>Login or Register</span>
+                      </button>
+                    );
+                  })}
               </div>
             </div>
           )}
 
           {/* <!-- Right Side Menu (Logged In) --> */}
-          {isLoggedIn && (
+          {session && (
             <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
               <Link href='/messages' className='relative group'>
                 <button
@@ -223,7 +244,7 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            {isLoggedIn && (
+            {session && (
               <Link
                 href='/properties/add'
                 className={`${
@@ -235,7 +256,7 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-            {!isLoggedIn && (
+            {!session && (
               <button className='flex items-center justify-center text-amber-300 font-semibold bg-amber-950 hover:bg-white hover:text-amber-950 rounded-md px-3 py-2 my-5 w-full duration-200'>
                 <FcGoogle className='mr-2 text-xl' />
                 <span>Login or Register</span>

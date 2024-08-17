9f1643c806a2e82e5706ac632cf8def8305112d1
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-17 15:02:14 +0300

    AddPropertyForm added

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import AddPropertyForm from '@/components/AddPropertyForm';
 
 const AddProjectPage = () => {
-  return <div>AddProjectPage</div>;
+  return (
+    <section className='bg-blue-50'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-8 shadow-md rounded-md border m-4 md:m-0'>
+          <AddPropertyForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddProjectPage;

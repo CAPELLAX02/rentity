56351e05bc1e0a5cb775283a726f9240ef579406
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-20 21:48:10 +0300

    featured projects component added in home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
+import FeaturedProperties from '@/components/FeaturedProperties';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -13,15 +13,15 @@ const HomeProperties = async () => {
 
   return (
     <>
-      <section className='px-4 py-6 bg-blue-50 my-3'>
-        <div className='container-xl lg:container m-auto px-4 py-6'>
-          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+      <section className="px-4 py-6 bg-amber-50 my-3">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-amber-500 mb-6 text-center">
             Recent Properties
           </h2>
           {recentProperties.length === 0 ? (
             <p>No properties found.</p>
           ) : (
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
               {recentProperties.map((property) => {
                 return <PropertyCard key={property._id} property={property} />;
               })}
@@ -30,10 +30,10 @@ const HomeProperties = async () => {
         </div>
       </section>
 
-      <section className='m-auto max-w-lg mb-12 px-14'>
+      <section className="m-auto max-w-lg mb-12 px-14">
         <Link
-          href='/properties'
-          className='block bg-blue-500 text-white font-semibold text-center py-3 px-5 rounded-xl text-xl hover:bg-amber-500 duration-200'
+          href="/properties"
+          className="block bg-amber-500 text-white font-semibold text-center py-3 px-5 rounded-xl text-xl hover:bg-blue-500 duration-200"
         >
           View All Properties
         </Link>
